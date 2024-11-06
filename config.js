5facb0a8dbbf5e94f829e50035332272c4a6ba92
alexKidszone <alex.lima@kidszoneworld.com.br>
2024-11-06 09:37:20 -0300

    atualizando bloqueio na admin

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js (com hash da senha)
+const credentials = {
+    login: "ti.kz@kidszoneworld.com.br",
+    passwordHash: "7d35f3180d46ab772a48db9ca2775bcedd7cf34c55333e49095e8bd750327bb2" // hash da senha
+};
