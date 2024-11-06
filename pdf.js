2289906fdbaa6b70d0062b244275d7da2cdbe67e
alexKidszone <alex.lima@kidszoneworld.com.br>
2024-11-06 20:48:54 -0300

    ajuste pdf para landscape

--- pdf.js
+++ pdf.js
@@ -13,10 +13,10 @@ btPdfGeneration.addEventListener("click", () => {
 
     // Configuração do arquivo de PDF
     const options = {
-        margin: 0, // Sem margem
+        margin: [0,0,0,0],// Sem margem
         filename: 'pedido_venda',
-        html2canvas: { scale: 2, windowWidth: 1200 }, // Largura de 1200px
-        jsPDF: { unit: "px", format: [1200, 929], orientation: "portrait" }, // Tamanho da página ajustado
+        html2canvas: { scale: 2}, // Largura de 1200px
+        jsPDF: { unit: "mm", format: "a4", orientation: "landscape" }, // Tamanho da página ajustado
         pagebreak: { mode: 'avoid-all' } // Evita quebras de página
     };
 
